bcde9983665f06fb6737133f21d20ecaca241d1c
Alexandre Laurent <alexandre.laurent@st.com>
2014-03-04 17:33:30 +0100

    Clean & remove dead code

--- nlinum.el
+++ nlinum.el
@@ -37,8 +37,6 @@
 (defvar nlinum--width 2)
 (make-variable-buffer-local 'nlinum--width)
 
-;; (defvar nlinum--desc "")
-
 ;;;###autoload
 (define-minor-mode nlinum-mode
   "Toggle display of line numbers in the left margin (Linum mode).
@@ -53,7 +51,6 @@ Linum mode is a buffer-local minor mode."
   (remove-hook 'after-change-functions #'nlinum--after-change)
   (kill-local-variable 'nlinum--line-number-cache)
   (remove-overlays (point-min) (point-max) 'nlinum t)
-  ;; (kill-local-variable 'nlinum--ol-counter)
   (kill-local-variable 'nlinum--width)
   (when nlinum-mode
     (add-hook 'window-configuration-change-hook #'nlinum--setup-window nil t)
@@ -71,7 +68,6 @@ Linum mode is a buffer-local minor mode."
 
 (defun nlinum--flush ()
   (nlinum--setup-windows)
-  ;; (kill-local-variable 'nlinum--ol-counter)
   (remove-overlays (point-min) (point-max) 'nlinum t)
   (run-with-timer 0 nil
                   (lambda (buf)
@@ -81,52 +77,6 @@ Linum mode is a buffer-local minor mode."
                          (point-min) (point-max) '(fontified)))))
                   (current-buffer)))
 
-;; (defun nlinum--ol-count ()
-;;   (let ((i 0))
-;;     (dolist (ol (overlays-in (point-min) (point-max)))
-;;       (when (overlay-get ol 'nlinum) (incf i)))
-;;     i))
-
-;; (defvar nlinum--ol-counter 100)
-;; (make-variable-buffer-local 'nlinum--ol-counter)
-
-;; (defun nlinum--flush-overlays (buffer)
-;;   (with-current-buffer buffer
-;;     (kill-local-variable 'nlinum--ol-counter)
-;;     ;; We've created many overlays in this buffer, which can slow
-;;     ;; down operations significantly.  Let's flush them.
-;;     ;; An easy way to flush them is
-;;     ;;   (remove-overlays min max 'nlinum t)
-;;     ;;   (put-text-property min max 'fontified nil)
-;;     ;; but if the visible part of the buffer requires more than
-;;     ;; nlinum-overlay-threshold overlays, then we'll inf-loop.
-;;     ;; So let's be more careful about removing overlays.
-;;     (let ((windows (get-buffer-window-list nil nil t))
-;;           (start (point-min))
-;;           (debug-count (nlinum--ol-count)))
-;;       (with-silent-modifications
-;;         (while (< start (point-max))
-;;           (let ((end (point-max)))
-;;             (dolist (window windows)
-;;               (cond
-;;                ((< start (1- (window-start window)))
-;;                 (setq end (min (1- (window-start window)) end)))
-;;                ((< start (1+ (window-end window)))
-;;                 (setq start (1+ (window-end window))))))
-;;             (when (< start end)
-;;               (remove-overlays start end 'nlinum t)
-;;               ;; Warn jit-lock that this part of the buffer is not done any
-;;               ;; more.  This has the downside that font-lock will be re-applied
-;;               ;; as well.  But jit-lock doesn't know how to (and doesn't want
-;;               ;; to) keep track of the status of its various
-;;               ;; clients independently.
-;;               (put-text-property start end 'fontified nil)
-;;               (setq start (+ end 1))))))
-;;       (let ((debug-new-count (nlinum--ol-count)))
-;;         (message "Flushed %d overlays, %d remaining"
-;;                  (- debug-count debug-new-count) debug-new-count)))))
-
-
 (defvar nlinum--line-number-cache nil)
 (make-variable-buffer-local 'nlinum--line-number-cache)
 
@@ -135,7 +85,6 @@ Linum mode is a buffer-local minor mode."
 
 (defun nlinum--line-number-at-pos ()
   "Like `line-number-at-pos' but sped up with a cache."
-  ;; (assert (bolp))
   (let ((pos
          (if (and nlinum--line-number-cache
                   (> (- (point) (point-min))
@@ -145,7 +94,6 @@ Linum mode is a buffer-local minor mode."
                       (cdr nlinum--line-number-cache)
                       (count-lines (point) (car nlinum--line-number-cache)))
            (line-number-at-pos))))
-    ;;(assert (= pos (line-number-at-pos)))
     (setq nlinum--line-number-cache (cons (point) pos))
     pos))
 
@@ -180,13 +128,8 @@ should be at least equal to `nlinum--width'.")
                    (overlay-put ol 'before-string
                                 (propertize " " 'display
                                             `((margin left-margin) ,str)))
-                   ;; (setq nlinum--ol-counter (1- nlinum--ol-counter))
-                   ;; (when (= nlinum--ol-counter 0)
-                   ;;   (run-with-idle-timer 0.5 nil #'nlinum--flush-overlays
-                   ;;                        (current-buffer)))
                    (setq line (1+ line))
                    (zerop (forward-line 1))))))))
-  ;; (setq nlinum--desc (format "-%d" (nlinum--ol-count)))
   nil)
 
 ;;;###autoload
@@ -196,20 +139,20 @@ should be at least equal to `nlinum--width'.")
 ;;;; ChangeLog:
 
 ;; 2014-01-02  Stefan Monnier  <monnier@iro.umontreal.ca>
-;; 
+;;
 ;; 	* nlinum.el: Add global-nlinum-mode and nlinum-format-function.
-;; 
+;;
 ;; 2012-10-24  Stefan Monnier  <monnier@iro.umontreal.ca>
-;; 
+;;
 ;; 	* nlinum.el: Speed up by caching last line-number.
 ;; 	(nlinum--line-number-cache): New var.
 ;; 	(nlinum--after-change, nlinum--line-number-at-pos): New functions.
 ;; 	(nlinum-mode, nlinum--region): Use them.
-;; 
+;;
 ;; 2012-06-08  Stefan Monnier  <monnier@iro.umontreal.ca>
-;; 
+;;
 ;; 	Add nlinum.el
-;; 
+;;
 
 
 (provide 'nlinum)
